1b6edacc3228d34777f645f6a0d6d5190dacbb5c
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2023-07-03 05:40:43 +0300

    Add get topic endpoint

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -37,6 +37,47 @@ exports.getAllTopics = async (req, res, next) => {
   }
 };
 
+exports.getTopic = async (req, res, next) => {
+  try {
+    const id = req.body.id;
+    if (!id) {
+      res.status(400).json({ message: "id cannot be null" });
+      return;
+    }
+    const topic = await Topic.findByPk(id);
+    if (!topic) {
+      res.status(404).json({ message: "Topic not found" });
+      return;
+    }
+    const result = {};
+    result.name = topic.name;
+    result.description = topic.description;
+    result.cfProblems = [];
+    result.lcProblems = [];
+    result.hrProblems = [];
+
+    const topicProblems = await TopicProblems.findAll({
+      where: { topicId: id },
+    });
+    for (const problem of topicProblems) {
+      const problemObj = await Problem.findByPk(problem.problemId);
+      if (problemObj.platform == "cf") {
+        result.cfProblems.push(problemObj);
+      } else if (problemObj.platform == "lc") {
+        result.lcProblems.push(problemObj);
+      } else if (problemObj.platform == "hr") {
+        result.hrProblems.push(problemObj);
+      }
+    }
+    result.res.status(200).json({ topic: result });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
 exports.addTopic = async (req, res, next) => {
   try {
     const name = req.body.name;
