cec9e8b4eb0774e0bd52e2d482d0336053bda13c
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2023-07-03 05:41:51 +0300

    Add get Topic to routes

--- routes/topic.js
+++ routes/topic.js
@@ -5,6 +5,7 @@ const topicController = require("../controllers/topicController");
 const router = express.Router();
 
 router.get("/all-topics", topicController.getAllTopics);
+router.get("/topic", topicController.getTopic);
 router.post("/add-topic", topicController.addTopic);
 
 module.exports = router;
